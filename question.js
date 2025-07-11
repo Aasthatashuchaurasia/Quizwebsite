61844788cc916de2a9ea28a053827dca29edbc47
Aastha Chaurasia <aasthachaurasia9876@gmail.com>
2025-07-11 13:15:59 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+        numb: 1,
+        question:"What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options:[
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+
+        ]
+    },
+
+    {
+        numb: 2,
+        question:"What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options:[
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet"
+
+        ]
+    },
+
+    {
+        numb: 3,
+        question:"What does PHP stand for?",
+        answer: "A. Hypertext Processor",
+        options:[
+            "A. Hypertext Processor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+
+        ]
+    },
+
+    {
+        
+            numb: 4,
+            question:"Which symbol is used to end a statement in C/C++?",
+            answer: "C. ; (semicolon)",
+            options:[
+              "A. . (dot)",
+              "B. : (colon)",
+              "C. ; (semicolon)",  // 🛠️ Fixed spacing
+              "D. , (comma)"
+            
+        ]
+    },
+
+    {
+        numb: 5,
+        question:" Which of the following is a feature of Object-Oriented Programming?",
+        answer: "A. Encapsulation",
+        options:[
+            "A. Encapsulation",
+            "B. Recursion",
+            "C. Compilation",
+            "D. Pointers"
+        ]
+    },
+
+    {
+        numb: 6,
+        question:"Which of these is not a valid access modifier in Java?",
+        answer: "C. internal",
+        options:[
+            "A. public",
+            "B. protected",
+            "C. internal",
+            "D.  private"
+        ]
+    },
+
+    {
+        numb: 7,
+        question:"What is the main purpose of the virtual keyword in C++?",
+        answer: "B. To enable dynamic polymorphism",
+        options:[
+            "A. To create global functions",
+            "B. To enable dynamic polymorphism",
+            "C. To protect data members",
+            "D.  To allocate memory dynamically"
+        ]
+    },
+
+    {
+        numb: 8,
+        question:"Which language is primarily used for iOS app development?",
+        answer: "C. Swift",
+        options:[
+            "A. Kotlin",
+            "B. Java",
+            "C. Swift",
+            "D. C#"
+        ]
+    },
+
+    {
+        numb: 9,
+        question:"What is the purpose of the pubspec.yaml file in a Flutter project? ?",
+        answer: "C. To list dependencies and assets",
+        options:[
+            "A. To define UI layout",
+            "B. To manage app permissions",
+            "C. To list dependencies and assets",
+            "D. To write Dart logic"
+        ]
+    },
+
+];
